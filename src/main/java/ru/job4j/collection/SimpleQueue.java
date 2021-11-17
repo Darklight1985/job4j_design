290adc9a2d0368127567aa03888053c9d2d33c81
Dmitrij Kolesnev <darklight1985@mail.ru>
2021-11-17 21:28:46 +0300

    5. Очередь на двух стеках [#160 #229622] исправил

--- src/main/java/ru/job4j/collection/SimpleQueue.java
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -1,11 +1,16 @@
 package ru.job4j.collection;
 
+import java.util.NoSuchElementException;
+
 public class SimpleQueue<T> {
     private final SimpleStack<T> in = new SimpleStack<>();
     private final SimpleStack<T> out = new SimpleStack<>();
     private int size = 0;
 
     public T poll() {
+        if (size == 0) {
+            throw new NoSuchElementException();
+        }
         size--;
         return out.pop();
     }
