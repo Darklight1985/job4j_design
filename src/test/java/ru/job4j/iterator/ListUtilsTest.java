b62f5192954634bd51ab172bd312075610861ad6
Dmitrij Kolesnev <darklight1985@mail.ru>
2021-11-20 22:24:29 +0300

    7. ListIterator [#350217 #229923] исправил3

--- src/test/java/ru/job4j/iterator/ListUtilsTest.java
+++ src/test/java/ru/job4j/iterator/ListUtilsTest.java
@@ -24,12 +24,11 @@ public class ListUtilsTest {
         ListUtils.addBefore(input, 3, 2);
     }
 
-    @Test
+    @Test(expected = IndexOutOfBoundsException.class)
     public void whenAddAfterLast() {
         List<Integer> input = new ArrayList<>(Arrays.asList(0, 1, 2));
-        ListUtils.addAfter(input, 2, 3);
-        assertThat(input, is(Arrays.asList(0, 1, 2, 3)));
-    }
+        ListUtils.addAfter(input, 3, 3);
+        assertThat(input, is(Arrays.asList(0, 1, 2, 3)));    }
 
     @Test
     public void whenPredicateRemove() {
