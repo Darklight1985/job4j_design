13f6b3345c66d73d4f64539df27d4e516a0c1e25
Dmitrij Kolesnev <darklight1985@mail.ru>
2021-11-22 22:27:06 +0300

    2. Статистика по коллекции. [#45889]

---
+++ src/main/java/ru/job4j/question/Analize.java
@@ -0,0 +1,40 @@
+package ru.job4j.question;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Analize {
+
+    public static Info diff(Set<User> previous, Set<User> current) {
+        int added = 0;
+        int changed = 0;
+        int deleted = 0;
+        List<User> previousList = previous.stream().collect(Collectors.toList());
+        List<User> currentList = current.stream().collect(Collectors.toList());
+        Collections.sort(currentList);
+        Collections.sort(previousList);
+       Iterator<User> userIteratorCur = currentList.iterator();
+        Iterator<User> userIteratorPrev = previousList.iterator();
+
+      while (userIteratorCur.hasNext()) {
+          User userCurEl = userIteratorCur.next();
+          if (userCurEl.getId() > 3) {
+              added++;
+          }
+          while (userIteratorPrev.hasNext()) {
+              User userPrevEl = userIteratorPrev.next();
+              if (userPrevEl == userCurEl) {
+                  break;
+              } else {
+                  if (userPrevEl.getId() == userCurEl.getId()) {
+                      changed++;
+                      break;
+                  } else {
+                      deleted++;
+                  }
+              }
+          }
+      }
+        return new Info(added, changed, deleted);
+    }
+}
