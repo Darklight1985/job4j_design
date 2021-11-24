278a22f986d3c73bbc345bce68d43fdddd6de689
Dmitrij Kolesnev <darklight1985@mail.ru>
2021-11-24 20:40:57 +0300

    2. Статистика по коллекции. [#45889 #231759] исправил 5

--- src/main/java/ru/job4j/question/Analize.java
+++ src/main/java/ru/job4j/question/Analize.java
@@ -9,23 +9,27 @@ public class Analize {
         int added = 0;
         int changed = 0;
         int deleted;
+        int notchanged = 0;
 
         Map<Integer, String> previousList = previous.stream()
                 .collect(Collectors
                         .toMap(User::getId, User::getName, (o1, o2) -> o1, LinkedHashMap::new));
 
         for (User user : current) {
-                if (!previousList.containsKey(user.getId())) {
-                    added++;
-                }
+            if (!previousList.containsKey(user.getId())) {
+                added++;
+            } else {
                 if (previousList.containsKey(user.getId())
                         && !previousList.containsValue(user.getName())
                         && !previousList.remove(user.getId(), user.getName())) {
                     changed++;
+                } else {
+                    notchanged++;
                 }
             }
+        }
 
-        deleted = Math.abs(Math.abs(previousList.size() - current.size()) - added);
+        deleted = previousList.size() - notchanged - changed;
 
         return new Info(added, changed, deleted);
     }
