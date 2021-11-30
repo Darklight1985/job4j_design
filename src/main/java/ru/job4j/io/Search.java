f2f15a2e712d0f7e360bab6665ffaa007b9dd954
Dmitrij Kolesnev <darklight1985@mail.ru>
2021-11-30 19:01:05 +0300

    5. Валидация параметров запуска. [#246865 #234166] исправил2

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,5 +1,6 @@
 package ru.job4j.io;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -9,17 +10,24 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        if (args.length == 0) {
-            throw new IllegalArgumentException("Root folder is null. "
-                    + "Usage java -jar dir.jar ROOT_FOLDER.");
+         validate(args);
+            Path start = Paths.get(args[0]);
+            search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
         }
-        Path start = Paths.get(args[0]);
-        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
-    }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
     }
-}
+
+    public static void validate(String[] args) {
+        File file = new File(args[0]);
+        if (file.length() == 0 || !file.isDirectory()
+                || !file.exists() || args.length != 2) {
+                throw new IllegalArgumentException("Root folder is null. "
+                        + "Usage java -jar dir.jar ROOT_FOLDER.");
+
+        }
+    }
+    }
