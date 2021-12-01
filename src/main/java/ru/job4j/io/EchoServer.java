89c0bd2274f82d4b1a7553813b97962e6fbfd41d
Dmitrij Kolesnev <darklight1985@mail.ru>
2021-12-02 00:06:03 +0300

    1. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -3,9 +3,12 @@ package ru.job4j.io;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EchoServer {
     public static void main(String[] args) throws IOException {
+        List<String> list = new ArrayList<>();
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
@@ -13,17 +16,22 @@ public class EchoServer {
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                    out.write("Hello, dear friend.".getBytes());
                     for (String str = in.readLine(); str != null && !str.isEmpty();
                          str = in.readLine()) {
-                        if ("GET /?msg=Bye HTTP/1.1".equals(str)) {
-                            server.close();
-                        }
+                        list.add(str);
                         System.out.println(str);
+
+                    }
+                    switch (list.get(0)) {
+                        case "GET /?msg=Hello HTTP/1.1" -> out.write("Hello".getBytes());
+                        case "GET /?msg=Exit HTTP/1.1" -> server.close();
+                        default -> out.write("What".getBytes());
                     }
                     out.flush();
+                }
+                        list.clear();
                 }
             }
         }
     }
-}
+
