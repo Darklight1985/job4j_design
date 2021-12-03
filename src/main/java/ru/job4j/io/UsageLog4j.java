81ec6f2f68db2ba67d23c85a12e7c98824cecd1a
Dmitrij Kolesnev <darklight1985@mail.ru>
2021-12-03 20:29:24 +0300

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -13,5 +13,17 @@ public class UsageLog4j {
         LOG.info("info message");
         LOG.warn("warn message");
         LOG.error("error message");
+        int age = 25;
+        double weight = 99.9;
+        byte three = 3;
+        short chislo = 2000;
+        float height = 178.2f;
+        boolean pravda = true;
+        long rasstoyanie = 2562L;
+        char symbol = 'D';
+        LOG.info("User {} , {} age old, with height = {}, weight = {},"
+                + " have {} cars, and living {} km from Mexico,"
+                + " its {}, this {}", symbol, age, height, weight,
+                three, rasstoyanie, pravda, chislo);
     }
 }
