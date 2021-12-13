15121aa827ed8574792a2a7c540068e6ae11b346
Dmitrij Kolesnev <darklight1985@mail.ru>
2021-12-13 19:09:41 +0300

    0.1. Statement [#379306]

---
+++ src/main/java/ru/job4j/jdbc/TableEditor.java
@@ -0,0 +1,158 @@
+package ru.job4j.jdbc;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.*;
+import java.util.Properties;
+import java.util.StringJoiner;
+
+public class TableEditor implements AutoCloseable {
+
+    private Connection connection;
+
+    private Properties properties;
+
+    public TableEditor(Properties properties) {
+        this.properties = properties;
+        initConnection();
+    }
+
+    private void initConnection() {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        String url = properties.getProperty("hibernate.connection.url");
+        String login = properties.getProperty("hibernate.connection.username");
+        String password = properties.getProperty("hibernate.connection.password");
+        try {
+            Connection connection = DriverManager.getConnection(url, login, password);
+            this.connection = connection;
+        DatabaseMetaData metaData = connection.getMetaData();
+        System.out.println(metaData.getUserName());
+            System.out.println(metaData.getURL());
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public void createTable(String tableName) {
+            try (Statement statement = connection.createStatement()) {
+                String sql = String.format(
+                        "create table if not exists " + tableName + " (%s);",
+                        "id serial primary key"
+                );
+                statement.execute(sql);
+                System.out.println(getTableScheme(connection, tableName));
+            } catch (Exception throwables) {
+                throwables.printStackTrace();
+            }
+    }
+
+    public void dropTable(String tableName) {
+        try (Statement statement = connection.createStatement()) {
+            String sql = String.format(
+                    "drop table %s;",
+                    tableName
+            );
+            statement.execute(sql);
+            System.out.println("Таблица " + tableName + " удалена");
+            close();
+        } catch (Exception throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public void addColumn(String tableName, String columnName, String type) {
+        try (Statement statement = connection.createStatement()) {
+            String sql = String.format(
+                    "alter table %s add %s  %s;",
+                    tableName,
+                    columnName,
+                    type
+            );
+            statement.execute(sql);
+            System.out.println(getTableScheme(connection, tableName));
+        } catch (Exception throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public void dropColumn(String tableName, String columnName) {
+        try (Statement statement = connection.createStatement()) {
+            String sql = String.format(
+                    "alter table %s drop column %s;",
+                    tableName,
+                    columnName
+            );
+            statement.execute(sql);
+            System.out.println(getTableScheme(connection, tableName));
+        } catch (Exception throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public void renameColumn(String tableName, String columnName, String newColumnName) {
+        try (Statement statement = connection.createStatement()) {
+            String sql = String.format(
+                    "alter table %s rename column %s to %s;",
+                    tableName,
+                    columnName,
+                    newColumnName
+            );
+            statement.execute(sql);
+            System.out.println(getTableScheme(connection, tableName));
+        } catch (Exception throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public static String getTableScheme(Connection connection, String tableName) throws Exception {
+        var rowSeparator = "-".repeat(30).concat(System.lineSeparator());
+        var header = String.format("%-15s|%-15s%n", "NAME", "TYPE");
+        var buffer = new StringJoiner(rowSeparator, rowSeparator, rowSeparator);
+        buffer.add(header);
+        try (var statement = connection.createStatement()) {
+            var selection = statement.executeQuery(String.format(
+                    "select * from %s limit 1", tableName
+            ));
+            var metaData = selection.getMetaData();
+            for (int i = 1; i <= metaData.getColumnCount(); i++) {
+                buffer.add(String.format("%-15s|%-15s%n",
+                        metaData.getColumnName(i), metaData.getColumnTypeName(i))
+                );
+            }
+        }
+        return buffer.toString();
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (connection != null) {
+            connection.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        File file = new File("app.properties");
+        Properties properties = new Properties();
+        try {
+            properties.load(new FileReader(file));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        TableEditor tableEditor = new TableEditor(properties);
+        tableEditor.createTable("demo");
+        tableEditor.addColumn("demo", "name", "varchar(255)");
+        tableEditor.renameColumn("demo", "name", "surname");
+        tableEditor.dropColumn("demo", "surname");
+        tableEditor.dropTable("demo");
+        try {
+            tableEditor.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
