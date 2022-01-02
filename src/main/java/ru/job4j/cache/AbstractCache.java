63ffae7dffccf86c7acc83b022ca9a653c698e66
Dmitrij Kolesnev <darklight1985@mail.ru>
2022-01-02 20:03:49 +0300

    1. Реализация кеша на SoftReference [#1592 #245954] исправил

--- src/main/java/ru/job4j/cache/AbstractCache.java
+++ src/main/java/ru/job4j/cache/AbstractCache.java
@@ -4,20 +4,20 @@ import java.lang.ref.SoftReference;
 import java.util.HashMap;
 import java.util.Map;
 
+import static java.util.Objects.nonNull;
+
 public abstract class AbstractCache<K, V> {
 
     private final Map<K, SoftReference<V>> cache = new HashMap<>();
 
     public void put(K key, V value) {
-     cache.put(key, new SoftReference<>(value));
+        if (nonNull(value) && !" ".equals(value)) {
+            cache.put(key, new SoftReference<>(value));
+        }
     }
 
     public V get(K key) {
-        V rsl = null;
-        if (cache.containsKey(key)) {
-            rsl = cache.get(key).get();
-        }
-        return rsl;
+        return cache.getOrDefault(key, new SoftReference<>(null)).get();
     }
 
     protected abstract V load(K key);
--- src/main/java/ru/job4j/cache/DirFileCache.java
+++ src/main/java/ru/job4j/cache/DirFileCache.java
@@ -1,9 +1,8 @@
 package ru.job4j.cache;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class DirFileCache extends AbstractCache<String, String> {
 
@@ -15,17 +14,13 @@ public class DirFileCache extends AbstractCache<String, String> {
 
     @Override
     protected String load(String key) {
-          StringBuilder stringBuilder = new StringBuilder();
-        try (BufferedReader buff = new BufferedReader(new FileReader(cachingDir + key))) {
-            while (buff.ready()) {
-             stringBuilder.append(buff.readLine());
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+          String rsl = null;
+        try {
+            rsl = Files.readString(Path.of(cachingDir, key));
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return String.valueOf(stringBuilder);
+        return rsl;
     }
 
 }
