03ff33ca9750c88ae4ef9e5234b0a39af76a1638
Dmitrij Kolesnev <darklight1985@mail.ru>
2022-01-06 12:37:37 +0300

    2. Что такое TDD? [#4918 #247288] исправил 2

--- src/test/java/ru/job4j/tdd/CinemaTest.java
+++ src/test/java/ru/job4j/tdd/CinemaTest.java
@@ -1,5 +1,6 @@
 package ru.job4j.tdd;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -12,6 +13,7 @@ import static org.junit.Assert.assertThat;
 
 public class CinemaTest {
 
+    @Ignore
     @Test
     public void buy() {
         Account account = new AccountCinema();
@@ -22,6 +24,38 @@ public class CinemaTest {
         assertThat(ticket, is(new Ticket3D()));
     }
 
+    @Ignore
+    @Test (expected = IllegalArgumentException.class)
+    public void buyNoValidDate() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 02, 30, 12, 05);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+    }
+
+    @Ignore
+    @Test (expected = IllegalArgumentException.class)
+    public void buyOccup() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 02, 12, 12, 05);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+        Ticket ticketTwo = cinema.buy(account, 1, 1, date);
+    }
+
+    @Ignore
+    @Test (expected = IllegalArgumentException.class)
+    public void buyNoValidPlace() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 02, 30, 12, 05);
+        Ticket ticket = cinema.buy(account, 5, 13, date);
+    }
+
+    @Ignore
     @Test
     public void find() {
         Cinema cinema = new Cinema3D();
@@ -30,6 +64,7 @@ public class CinemaTest {
         assertThat(sessions, is(Arrays.asList(new Session3D())));
     }
 
+    @Ignore
     @Test
     public void add() {
         Session session = new Session3D();
