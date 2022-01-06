68d118cfe5b3230604386b10a3d68f1997a1fdd7
Dmitrij Kolesnev <darklight1985@mail.ru>
2022-01-06 21:42:05 +0300

    3. Шаблонизатор. [#855]

---
+++ src/test/java/ru/job4j/template/GeneratorTest.java
@@ -0,0 +1,70 @@
+package ru.job4j.template;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class GeneratorTest {
+
+    @Ignore
+    @Test
+    public void produce() {
+        Generator generator = new GeneratorStr();
+        Map<String, String> mapStr = new HashMap<>();
+        mapStr.put("name", "Vasya");
+        mapStr.put("subject", "cat");
+        String template = "I am a ${name}, Who are ${subject}?";
+        assertThat(generator.produce(template, mapStr), is("I am a ${name}, Who are ${subject}? "));
+    }
+
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
+    public void nonCorrectTempl() {
+        Generator generator = new GeneratorStr();
+        Map<String, String> mapStr = new HashMap<>();
+        mapStr.put("name", "Vasya");
+        mapStr.put("job", "cat");
+        String template = "I am ${name}, I am ${age} years old and I work as an ${job}";
+        String rsl = generator.produce(template, mapStr);
+    }
+
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
+    public void nonCorrectArg() {
+        Generator generator = new GeneratorStr();
+        Map<String, String> mapStr = new HashMap<>();
+        mapStr.put("name", "Vasya");
+        mapStr.put("subject", "1");
+        String template = "I am a ${name}, Who are ${subject}?";
+        String rsl = generator.produce(template, mapStr);
+    }
+
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
+    public void extraArg() {
+        Generator generator = new GeneratorStr();
+        Map<String, String> mapStr = new HashMap<>();
+        mapStr.put("name", "Vasya");
+        mapStr.put("subject", "cat");
+        mapStr.put("job", "engineer");
+        String template = "I am a ${name}, Who are ${subject}?";
+        String rsl = generator.produce(template, mapStr);
+    }
+
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
+    public void nullArg() {
+        Generator generator = new GeneratorStr();
+        Map<String, String> mapStr = new HashMap<>();
+        mapStr.put("name", "Vasya");
+        mapStr.put("age", "null");
+        mapStr.put("job", "engineer");
+        String template = "I am ${name}, I am ${age} years old and I work as an ${job}";
+        String rsl = generator.produce(template, mapStr);
+    }
+}
