80aa91888a21152ffd1f32ea22c5b259a4b9b9a6
Dmitrij Kolesnev <darklight1985@mail.ru>
2022-01-07 16:14:15 +0300

    1. Принципы Kiss, Dry и Yagni [#238813 #247264] исправил 3

--- src/main/java/ru/job4j/kiss/MaxMin.java
+++ src/main/java/ru/job4j/kiss/MaxMin.java
@@ -5,37 +5,21 @@ import java.util.List;
 import java.util.function.Predicate;
 
 public class MaxMin {
-    public <T> T findBy(List<T> value, Comparator<T> comparator, Predicate<T> predicate) {
-        T rsl = null;
-        boolean needIter = true;
-        while (needIter) {
-            needIter = false;
-            for (int i = 1; i < value.size(); i++) {
-                if (comparator.compare(value.get(i), value.get(i - 1)) > 0) {
-                    swap(value, i, i - 1);
-                    needIter = true;
+    public <T> T findBy(List<T> value, Comparator<T> comparator, Predicate<Integer> predicate) {
+        T rsl = value.get(0);
+            for (T t: value) {
+                if (predicate.test(comparator.compare(rsl, t))) {
+                    rsl = t;
                 }
-            }
-        }
-        for (T t: value) {
-            if (predicate.test(t)) {
-                rsl = t;
-            }
             }
             return rsl;
         }
 
     public <T> T max(List<T> value, Comparator<T> comparator) {
-         return findBy(value, comparator, T -> T == value.get(0));
+         return findBy(value, comparator, s -> s < 0);
     }
 
     public <T> T min(List<T> value, Comparator<T> comparator) {
-        return findBy(value, comparator, T -> T == value.get(value.size() - 1));
-    }
-
-    private <T> void swap(List<T> value, int elem1, int elem2) {
-        T tmp = value.get(elem1);
-        value.set(elem1, value.get(elem2));
-        value.set(elem2, tmp);
+        return findBy(value, comparator, s -> s > 0);
     }
 }
