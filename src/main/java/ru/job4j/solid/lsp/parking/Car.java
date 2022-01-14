211c90601541fc5770488c0cdf9012afbaa127bd
Dmitrij Kolesnev <darklight1985@mail.ru>
2022-01-14 23:20:09 +0300

    2. Парковка машин [#853 #250341] исправил2

--- src/main/java/ru/job4j/solid/lsp/parking/Car.java
+++ src/main/java/ru/job4j/solid/lsp/parking/Car.java
@@ -4,11 +4,6 @@ public class Car {
    private String number;
    private int size;
 
-    public Car(String number, int size) {
-        this.number = number;
-        this.size = size;
-    }
-
     public String getNumber() {
         return number;
     }
